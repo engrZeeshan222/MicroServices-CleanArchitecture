83dc777fbecf2ceb7adcad2ae53bca860af8c55d
Engr Zeeshan <zeeshan@icaremanager.com>
2024-08-13 18:38:08 +0500

    Code

--- CleanArchitecture-master/Clean.Application/Dtos/ResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Clean.Application.Dtos
-{
-    public class ResponseDto
-    {
-        public bool Status { get; set; } = false;
-        public string Message { get; set; }
-        public object Data { get; set; }
-    }
-}
